9adf337bf59c4ced8a0b873e03e88a215d5f007f
mauperez9918 <mauperez9918@gmail.com>
2023-09-16 01:21:10 -0300

    NewRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ItemListContainer from "./components/pages/itemList/ItemListContainer";
+import { BrowserRouter } from "react-router-dom";
 import CartContextComponent from "./contexts/CartContext";
-import CartContainer from "./components/pages/cart/CartContainer";
-import CheckOut from "./components/pages/checkout/CheckOut";
-import Layout from "./components/layout/Layout";
-import ItemDetailContainer from "./components/pages/itemDetail/ItemDetailContainer";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <>
       <CartContextComponent>
         <BrowserRouter>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<ItemListContainer />} />
-              <Route
-                path="/category/:categoryName"
-                element={<ItemListContainer />}
-              />
-              <Route path="/itemdetail/:id" element={<ItemDetailContainer />} />
-              <Route path="/cart" element={<CartContainer />} />
-              <Route path="/checkout" element={<CheckOut />} />
-            </Route>
-          </Routes>
+          <AppRouter />
         </BrowserRouter>
       </CartContextComponent>
     </>
---
+++ src/router/menuRoutes.js
@@ -0,0 +1,32 @@
+import CartContainer from "../components/pages/cart/CartContainer";
+import CheckOut from "../components/pages/checkout/CheckOut";
+import ItemDetailContainer from "../components/pages/itemDetail/ItemDetailContainer";
+import ItemListContainer from "../components/pages/itemList/ItemListContainer";
+
+export const menuRoutes = [
+  {
+    id: "home",
+    path: "/",
+    Element: ItemListContainer,
+  },
+  {
+    id: "category",
+    path: "/category/:categoryName",
+    Element: ItemListContainer,
+  },
+  {
+    id: "itemDetail",
+    path: "/itemdetail/:id",
+    Element: ItemDetailContainer,
+  },
+  {
+    id: "cart",
+    path: "/cart",
+    Element: CartContainer,
+  },
+  {
+    id: "checkout",
+    path: "/checkout",
+    Element: CheckOut,
+  },
+];
