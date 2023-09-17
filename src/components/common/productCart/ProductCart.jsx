f6f02a5e669ec246c67c12c4adc973870b2ea7af
mauperez9918 <mauperez9918@gmail.com>
2023-09-16 21:26:18 -0300

    fixes vercel

--- src/components/common/productCart/ProductCart.jsx
+++ src/components/common/productCart/ProductCart.jsx
@@ -1,5 +1,5 @@
 import { Box, Button, Grid, Typography } from "@mui/material";
-import "./productCart.css";
+import "./ProductCart.css";
 import { useContext } from "react";
 import { CartContext } from "../../../contexts/CartContext";
 import DeleteIcon from "@mui/icons-material/Delete";
