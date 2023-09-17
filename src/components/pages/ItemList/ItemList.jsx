f66face8e0c9148208e09f1352e99e4f900fca7a
mauperez9918 <mauperez9918@gmail.com>
2023-09-16 21:23:28 -0300

    fixes vercel

--- src/components/pages/ItemList/ItemList.jsx
+++ src/components/pages/ItemList/ItemList.jsx
@@ -1,5 +1,5 @@
 import { Box, Grid, Typography } from "@mui/material";
-import ProductCard from "../../common/productCard/ProductCard";
+import ProductCard from "../../common/ProductCard/ProductCard";
 
 const ItemList = ({ products, category }) => {
   return (
